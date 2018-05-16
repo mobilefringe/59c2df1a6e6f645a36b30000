bd8af0035eb99d86182ca76d74d634bf8bba300a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-16 11:43:49 -0400

    Carlingwood 2.0 1.1  /application.js

--- application.js
+++ application.js
@@ -432,6 +432,9 @@ function renderPromotions(container, template, collection, mall_name){
                 console.log(val.promo_image_url_abs.indexOf('missing.png') > -1);
                 val.image_url  = "//codecloud.cdn.speedyrails.net/sites/59c2df1a6e6f645a36b30000/image/png/1505943652000/Carlingwood_Logo_New.png";
             }
+            else {
+                
+            }
             
             val.store_name = mall_name;
             val.store_slug = "/"
