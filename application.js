8d7612744f1c2f7006136e565f74df557e20c50a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-08-22 09:53:26 -0400

    Carlingwood 2.0 1.1  /application.js

--- application.js
+++ application.js
@@ -32,7 +32,7 @@ function renderBanner(home_banner, banner_template, banners){
             }
         }
         
-        if(val.url == "" || val.url == null){
+        if(val.url === "" || val.url === null){
             val.show_url = "display: none";
         } else {
             val.show_url = "display: block";
