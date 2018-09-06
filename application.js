5e31e02935ee262cbd604e23e2110d04c3a7fa5a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-06 11:34:05 -0400

    Carlingwood 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -615,8 +615,8 @@ function renderSinglePost(container, template, main_post){
         main_post.post_image = main_post.image_url;
     }
         
-    if(main_post.body.length > 200){
-        main_post.description_short = main_post.body.substring(0,200) + "...";
+    if(main_post.body.length > 210){
+        main_post.description_short = main_post.body.substring(0,210) + "...";
     } else{
         main_post.description_short = main_post.body;
     }
