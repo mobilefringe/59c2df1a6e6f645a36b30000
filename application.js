e92ec8fdae76fd60e27c6c3968b2e0bf03d0aa02
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-03-25 12:47:36 -0400

    Carlingwood 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -540,7 +540,7 @@ function renderPosts(container, template, collection){
         }
         val.description_short = val.description_short.replace("&amp;", "&");
         
-        val.slug = "posts/" + val.slug;
+        // val.slug = "posts/" + val.slug;
         
         val.twitter_title = val.title + " via @CarlingwoodSCTR";
         
