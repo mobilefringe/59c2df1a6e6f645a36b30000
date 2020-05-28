4552bfa8352ac227c14f7779d50d802cf39a0a5b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-05-28 11:46:41 -0400

    Carlingwood 2.0 1.2  /helpers.js

--- helpers.js
+++ helpers.js
@@ -45,6 +45,8 @@ function init(e){
     //dynamically changing copyright year
     var current_year = moment().year();
     $("#current_year").text(current_year);
+    
+    getInsidePageBanner();
 }
 
 function getInsidePageBanner() {
